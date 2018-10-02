2f33582994938421c565fbbcee7a7bcf9d45b768
Christopher C Kenny <christopher.kenny1@gmail.com>
2018-10-02 14:36:34 -0400

    Initial creation of SleepIntro solution. Contains prompts for name and hours of sleep per "An Introduction to C# and .NET" module on PluralSight. Personally played around with additional class and programs to see what I could figure out before moving on. Console Application

---
+++ SleepIntro/SleepIntro/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SleepIntro
+{
+    class Actions
+    {
+        public static void Sleep(int hours)
+        {
+            if (hours > 7)
+            {
+                Console.WriteLine("You are well rested.");
+            }
+            else
+            {
+                Console.WriteLine("You need more rest.");
+            }
+        }
+    }
+    class Program
+    {
+        static string SleepChk(int hours)
+        {
+            string msg;
+
+            if (hours > 7)
+            {
+                msg = "You are well rested.";
+            }
+            else
+            {
+                msg = "You need more rest.";
+            }
+
+            return msg;
+        }
+
+        static void Main(string[] args)
+        {
+            string name;
+            string SlpMsg;
+
+            //Prompt user
+            Console.WriteLine("Your name:");
+            name = Console.ReadLine();
+
+            //Clear prompt and respond
+            Console.Clear();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+
+            //Clear response and prompt user
+            Console.Clear();
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            //Clear prompt and respond
+            Console.Clear();
+            //Actions.Sleep(hoursOfSleep);
+            SlpMsg = SleepChk(hoursOfSleep);
+
+            Console.WriteLine(SlpMsg);
+
+            //if (hoursOfSleep >= 8)
+            //{
+            //    Console.WriteLine("You are well rested.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Go back to sleep.");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
