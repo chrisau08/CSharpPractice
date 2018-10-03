9489b0d768178ee09473a859e2f9ed3d0cd6659b
Christopher C Kenny <christopher.kenny1@gmail.com>
2018-10-03 09:44:03 -0400

    Initial creation of Grades solution in conjunction with PluralSight module "Classes and Objects". Videos completed through "Classes and Variables".

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grades
+{
+    //Class GradeBook is being used as a blueprint for creating objects (or types). Consider this like an object in PL-SQL.
+    class GradeBook
+    {
+        //Constructor for the class GradeBook to instantiate or initialize the object.
+        public GradeBook()
+        {
+            grades = new List<float>(); //List that can hold 0 or more numbers
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+    }
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook(); //Variable that can be used to access the class GradeBook (variable of type GradeBook.
+            book.AddGrade(91);
+            book.AddGrade(89.5f); //f tells the compiler that the value is a float and not double.
+
+            GradeBook book2 = new GradeBook();
+            book2.AddGrade(75);
+        }
+    }
+}
