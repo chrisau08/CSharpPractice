f8e8ccddbe3b67b390b19f30327b3459e15ba2fb
Christopher C Kenny <christopher.kenny1@gmail.com>
2018-12-13 11:03:52 -0500

    Completed "Classes and Objects" module through beginning of "Statistics" so new code and comments have been added.

--- Grades/Grades/GradeBook.cs
+++ Grades/Grades/GradeBook.cs
@@ -7,17 +7,23 @@ namespace Grades
     //Class GradeBook is being used as a blueprint for creating objects (or types). Consider this like an object in PL-SQL.
     class GradeBook
     {
+        //static makes the object or function available without the code having to create a new instance of it.
+        public static float MinimumGrade = 0; //So this can be called in something like console.writeline as GradeBook.MinimumGrade without needing "new MinimumGrade"
+        public static float MaximumGrade = 100;
+
         //Constructor for the class GradeBook to instantiate or initialize the object.
         public GradeBook()
         {
             grades = new List<float>(); //List that can hold 0 or more numbers
         }
 
+        //This can actually be written as public void AddGrade(float grade) => grades.Add(grade);
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
+        //If you don't specify the access modifier (public, private, etc) then it is a private member (as opposed to public).
         List<float> grades;
     }
 }
--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -10,8 +10,13 @@ namespace Grades
             book.AddGrade(91);
             book.AddGrade(89.5f); //f tells the compiler that the value is a float and not double.
 
+            //This would create a new object, gradebook, in memory and assign book2 the memory location.
             GradeBook book2 = new GradeBook();
             book2.AddGrade(75);
+
+            GradeBook book3 = book; //This creates the book3 variable which will have the memory location of book assigned to it.
+            //Now any additions to book3 also impact book because they are referencing the same object at the specified memory location (Very Important).
+            book3.AddGrade(15);
         }
     }
 }
